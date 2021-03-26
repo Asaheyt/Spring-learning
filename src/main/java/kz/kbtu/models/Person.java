d2f84a711904dd16266615a1efce4ac71fd8ba3c
Adil Asaheyt Aldamzharov <Sarzhigitov.adil@gmail.com>
2021-03-26 15:15:44 +0600

    first commit

---
+++ src/main/java/kz/kbtu/models/Person.java
@@ -0,0 +1,35 @@
+package kz.kbtu.models;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+public class Person {
+    private int id;
+
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
+    private String name;
+
+    public Person() {}
+
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
